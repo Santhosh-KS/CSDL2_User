a3afba6f39fb6d3c362299f9398db8351781749d
Santhosh K S <santhoshachar08@gmail.com>
2023-06-26 00:19:17 +0200

    First SDL window

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,5 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 import CSDL2
-SDL_Quit()
-print("Hello, world!")
+
+let sdlInit = SDL_Init(SDL_INIT_VIDEO)
+let window:OpaquePointer = SDL_CreateWindow("Test From Swift", 30, 30, 640, 480, SDL_WINDOW_SHOWN.rawValue)!
+
+SDL_UpdateWindowSurface(window)
+SDL_Delay(2000)
+
+
+SDL_DestroyWindow(window)
